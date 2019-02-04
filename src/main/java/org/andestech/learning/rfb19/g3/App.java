a76f6641ee1d2617d3dcc6ea0b66e0da94d83c6f
VitalikMetallik <flash_PS4@mail.ru>
2019-02-04 13:58:46 +0300

    HomeWork2 Okovity Vitaly

--- src/main/java/org/andestech/learning/rfb19/g3/App.java
+++ src/main/java/org/andestech/learning/rfb19/g3/App.java
@@ -1,13 +1,12 @@
 package org.andestech.learning.rfb19.g3;
 
-/**
- * Hello world!
- *
- */
-public class App 
+
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        DebitAccount debitAccount = new DebitAccount("A", 100000, 265478412);
+        debitAccount.putMoney(100001);
+        debitAccount.withdrawal(66666);
     }
 }
